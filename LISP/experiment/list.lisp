0f29698babaab8082f6c18bb78d110ca11b13f96
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-04-14 23:51:28 -0700

    learn common lisp

---
+++ LISP/experiment/list.lisp
@@ -0,0 +1,88 @@
+
+;;;; Practical Common Lisp: A Simple Database
+
+
+;; list
+'(1 2 3)
+(list 1 2 3)
+
+;; property list (plist)
+(list :a 1 :b 2 :c 3)                   ;keyword start with colon
+;; use getf to get property
+(getf (list :a 1 :b 2 :c 3) :a)
+
+
+;; CD database
+
+(defun make-cd (title artist rating ripped)
+  (list :title title :artist artist :rating rating :ripped ripped))
+
+(defvar *db* nil)
+(defun add-record (cd) (push cd *db*))
+
+(add-record (make-cd "Roses" "Kathy Mattea" 7 t))
+(add-record (make-cd "Fly" "Dixie Chicks" 8 t))
+(add-record (make-cd "Home" "Dixie Chicks" 9 t))
+
+(defun dump-cd ()
+  (dolist (cd *db*)
+    (format t "~{~a:~10t~a~%~}~%" cd)))
+
+;; interactive input
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped [y/n]")))
+
+(defun add-cds ()
+  (loop (add-record (prompt-for-cd))
+     (if (not (y-or-n-p "Another? [y/n]: "))
+         (return))))
+
+(defun save-db (filename)
+  (with-open-file (out filename
+                       :direction :output
+                       :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
+
+(defun select-by-artist (artist)
+  (remove-if-not
+   #'(lambda (cd) (equal (getf cd :artist) artist))
+   *db*))
+
+(defun select (select-fn)
+  (remove-if-not select-fn *db*))
+
+(defun where (&key title artist rating (ripped nil ripped-p))
+  #'(lambda (cd)
+      (and
+       (if title (equal (getf cd :title) title) t)
+       (if artist (equal (getf cd :artist) artist) t)
+       (if rating (equal (getf cd :rating) rating) t)
+       (if ripped-p (equal (getf cd :ripped) ripped) t))))
+
+(select (where :artist "Dixie Chicks"))
+
+(defun update (select-fn &key title artist rating (ripped nil ripped-p))
+  (setf *db*
+        (mapcar
+         #'(lambda (row)
+             (when (funcall select-fn row)
+               (if title (setf (getf row :title) title))
+               (if artist (setf (getf row :artist) artist))
+               (if rating (setf (getf row :rating) rating))
+               (if ripped-p (setf (getf row :ripped) ripped)))
+             row) *db*)))
--- LISP/pe150.lisp
+++ LISP/pe150.lisp
@@ -2,12 +2,13 @@
 ;; Searching a triangular array for a sub-triangle having minimum-sum
 
 
-(defconstant +size+ 500500)
+(defconstant +size+  1000)
+(defconstant +total+ (/ (* +size+ (1+ +size+)) 2))
 
-(defvar random-numbers
-  (loop with x = (make-array +size+ :fill-pointer 0)
+(defconstant random-numbers
+  (loop with x = (make-array +total+ :fill-pointer 0)
         with tmp = 0
-        for k from 1 to +size+
+        for k from 1 to +total+
         do (progn
              (setf tmp (mod (+ 797807 (* tmp 615949)) (expt 2 20)))
              (vector-push (- tmp (expt 2 19)) x))
@@ -16,6 +17,9 @@
 ;; (print random-numbers)
 ;; (print (reduce '+ random-numbers))
 
+
+(defvar sum-cache
+  ())
 
 (defun min-sub-triangle-sum (triangular-array)
   ())
