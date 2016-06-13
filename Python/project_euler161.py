581b6e3c0f87df144e79443eb0cfece15d6100a2
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-06-13 01:56:54 -0700

    possible inifite recursion

--- Python/project_euler161.py
+++ Python/project_euler161.py
@@ -3,22 +3,33 @@
 """
 tile region with the following blocks
 
-form1: XXX
+type A
+XXX
 
-form2: XX
-       X
+type B
+XX
+X
+
+idea: propagate along columns
+
+0123456
+ ------>
+ ------>
+ ------>
 """
 
+from collections import defaultdict
 
-# hard-coded state proprogation
+
+# hard-coded state proprogation, not used in solving the real size problem
 class Tiling2:
 
     def __init__(self, n):
         if n % 3 != 0:
             return
         self.n = n
-        self.A = [0] * (n + 1)
-        self.B = [0] * (n + 1)
+        self.A = [0] * (n + 1)  # after putting a type A block
+        self.B = [0] * (n + 1)  # after putting a type B block
         self.A[0] = 1
 
     def solve(self):
@@ -43,126 +54,141 @@ class Tiling2:
             return self.B[n]
 
 
-class Tiling3:
+def fill_col(state):
+    if all(i > 0 for i in state):
+        return [state]
 
-    def __init__(self, n):
-        self.n = n
-        self.A = [0] * (n + 1)
-        self.B = [0] * (n + 1)
-        self.A[0] = 1
-
-    def solve(self):
-        return self.A_sub(self.n)
-
-    def A_sub(self, n):
-        """
-        ___
-        ___
-        ___
-
-        __X _XX
-        __X _X_
-        __X ___
-
-        _XX ___ __X ___
-        __X __X _XX _XX
-        ___ _XX ___ __X
-
-        XXX     XXX
-        ___ ==> XXX
-        ___     XXX
-        """
-        if n < 0:
-            return 0
-        elif self.A[n] != 0:
-            return self.A[n]
-        else:
-            self.A[n] = self.A_sub(n - 1) + self.A_sub(n - 2) + \
-                2 * self.B_sub(n) + 2 * self.B_sub(n - 1) + \
-                self.A_sub(n - 3)
-            return self.A[n]
-
-    def B_sub(self, n):
-        """
-        _XX
-        __X
-        ___
-        """
-        if n <= 0:
-            return 0
-        elif self.B[n] != 0:
-            return self.B[n]
-        else:
-            self.B[n] = self.A_sub(n - 2) + self.A_sub(n - 3) + \
-                self.B_sub(n - 3)
-            return self.B[n]
-
-
-class Tiling_n:
-    """
-    state at each stage is represented as n-element tuple
-    """
-
-    def __init__(self, n, m):
-        """n by m grid
-        propagate change along column
-        """
-        self.n = n
-        self.state_cache = {}
-        self.cache = {}
-
-    def solve(self):
-        """
-        can only propagate in one direction, because of 1 to many mapping
-        """
-
-    def fill_col(self, state):
-        if state in self.state_cache:
-            return self.state_cache[state]
-
-        after_states = []
-        finished = False
-        while not finished:
-            aft_state = list[state]
-            for index in range(self.n):
-                if aft_state[index] > 0:  # 1, 2
-                    continue
-                # horizontal form1 tile
-                aft_state[index] += 3
-
-            after_states.append(tuple(map(lambda x: x - 1, aft_state)))
-
-        self.state_cache[state] = after_states
-        return after_states
-
-
-def state_after_fill_col(index, state):
-    if index >= len(state):
-        return []
-
-    if state[index] > 0:    # state[index] = 1 or 2
-        return state_after_fill_col(index + 1, state)
-
-    # state[index] = 0
+    index = state.index(0)
     after_states = []
 
-    if index <= len(state) - 1:
-        aft_state = list(state)
-        aft_state[index] += 3
-        after_states.append(aft_state)
-    elif index <= len(state) - 2:
-        if state[index + 1] == 0:
-            aft_state = list(state)
+    # try:
+    #     index = state.index(0)
+    # except ValueError:
+    #     return [state]
+
+    # fill with type A
+    # XXX
+    after = list(state)
+    after[index] = 3
+    after_states.extend(fill_col(tuple(after)))
+    # X
+    # X
+    # X
+    if index < len(state)-2 and state[index+1] == 0 and state[index+2] == 0:
+        after = list(state)
+        after[index] = 1
+        after[index+1] = 1
+        after[index+2] = 1
+        after_states.extend(fill_col(tuple(after)))
+
+    # fill with type B, 1 to the left, 2 orientations
+    # orient up
+    #  X
+    # XX
+    if index > 0 and state[index-1] == 1:
+        after = list(state)
+        after[index] = 2
+        after[index-1] = 2
+        after_states.extend(fill_col(tuple(after)))
+
+    # orient down
+    # XX
+    #  X
+    if index < len(state)-1 and state[index+1] == 1:
+        after = list(state)
+        after[index] = 2
+        after[index+1] = 2
+        after_states.extend(fill_col(tuple(after)))
+    if index < len(state)-2 and state[index+1] == 0 and state[index+2] == 0:
+        # XX
+        # XX
+        # XX
+        after = list(state)
+        after[index] = 2
+        after[index+1] = 2
+        after[index+2] = 2
+        after_states.extend(fill_col(tuple(after)))
+    if index < len(state)-3 and state[index+1] == 0 and \
+            state[index+2] == 0 and state[index+3] == 0:
+        # XX
+        # XX
+        # X
+        # X
+        after = list(state)
+        after[index] = 2
+        after[index+1] = 2
+        after[index+2] = 1
+        after[index+3] = 1
+        after_states.extend(fill_col(tuple(state)))
+
+    # fill with type B, 2 to the left, 2 orientations
+    # orient up/down
+    if index < len(state)-1 and state[index+1] == 0:
+        # XX
+        # X
+        after = list(state)
+        after[index] = 2
+        after[index+1] = 1
+        after_states.extend(fill_col(tuple(after)))
+        # X
+        # XX
+        after[index] = 1
+        after[index+1] = 2
+        after_states.extend(fill_col(tuple(after)))
 
     return after_states
 
 
+class Tiling_n:
+    def __init__(self, n, m):
+        """n by m grid
+        propagate change along columns, in a increasing manner
+
+        each state is represented as a column, of size self.rows
+        """
+        self.rows = n
+        self.cols = m
+        self.state_cache = {}                   # state -> state
+        self.curr_col_cache = defaultdict(int)  # state -> count
+
+    def solve(self):
+        empty_state = (0, ) * self.rows
+        self.curr_col_cache[empty_state] = 1
+
+        for i in range(self.cols):
+            self._solve_col()
+            print(self.curr_col_cache)
+            print(self.state_cache)
+        return self.curr_col_cache[empty_state]
+
+    def _solve_col(self):
+        next_col_cache = defaultdict(int)
+
+        for state in self.curr_col_cache:
+            if all(i > 0 for i in state):
+                next_col_cache[tuple(i-1 for i in state)] += \
+                    self.curr_col_cache[state]
+            else:
+                for aft_state in self.fill_column(state):
+                    next_col_cache[tuple(i-1 for i in aft_state)] += \
+                        self.curr_col_cache[state]
+
+        self.curr_col_cache = next_col_cache
+
+    def fill_column(self, state):
+        if state in self.state_cache:
+            return self.state_cache[state]
+
+        after_states = fill_col(state)
+        self.state_cache[state] = after_states
+        return after_states
+
+
 def main():
     print(Tiling2(9).solve())
-    print(Tiling3(9).solve())
+    print(Tiling2(6).solve())
 
-    print(Tiling2(3).solve())
-    print(Tiling3(2).solve())
+    print(Tiling_n(6, 2).solve())
 
 
 if __name__ == '__main__':
