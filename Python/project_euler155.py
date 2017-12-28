f83b7ba834724787e0c23f394048474410f9b4d5
Xinyu Yang <colin.yangxinyu@outlook.com>
2017-12-28 15:54:55 +0800

    solve pe155 slow

---
+++ Python/project_euler155.py
@@ -0,0 +1,105 @@
+# Problem 155: Counting Capacitor Circuits
+
+from __future__ import print_function
+from __future__ import division
+
+from fractions import Fraction
+from itertools import product
+
+N = 18
+cache_split = {1: [[1]]}        # Dict[int, List[List[int]]]
+cache_parallel = {1: {1}}       # Dict[int, Set[int]]
+cache_serial = {1: {1}}         # Dict[int, Set[int]]
+
+
+def cached(cache):
+
+    def wrapped(func):
+        def inner(n):
+            if n not in cache:
+                cache[n] = func(n)
+            return cache[n]
+        return inner
+
+    return wrapped
+
+
+def _split(n, start=1):
+    if start > n:
+        return []
+
+    res = []
+    res.append([n])
+    for first in range(start, n//2+1):
+        for latter in _split(n-first, first):
+            latter.append(first)
+            res.append(latter)
+    return res
+
+
+split = cached(cache_split)(_split)
+
+
+@cached(cache_parallel)
+def parallel(n):
+    if n == 1:
+        return {1}
+
+    res = set()
+    for splitted in split(n):
+        if len(splitted) == 1:
+            # special case, not splitting at all
+            res.add(splitted[0])
+            continue
+        each_series = [serial(i) for i in splitted]
+        for each in product(*each_series):
+            res.add(sum(each))
+
+    return res
+
+
+@cached(cache_serial)
+def serial(n):
+    if n == 1:
+        return {1}
+
+    res = set()
+    for splitted in split(n):
+        if len(splitted) == 1:
+            # special case, not splitting at all
+            res.add(splitted[0])
+            continue
+        each_parallel = [parallel(i) for i in splitted]
+        for each in product(*each_parallel):
+            res.add(
+                Fraction(1, sum(Fraction(1, i) for i in each))
+            )
+
+    return res
+
+
+def d_exact(n):
+    # using exactly n capacitors
+    return len(parallel(n) | serial(n))
+
+
+def d(n):
+    # using up to n capacitors
+    values = set()
+    for i in range(1, n+1):
+        exactly = parallel(i) | serial(i)
+        # print(exactly)
+        values |= exactly
+    return len(values)
+
+
+def main():
+    # print(d(1))
+    # print(d(2))
+    # print(d(3))
+
+    print(d(N))
+
+
+if __name__ == '__main__':
+    main()
