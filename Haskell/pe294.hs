45f9dd29b452aa00d8cc094771b0e7861afe3406
Colin Yang <colinxy@linux.ucla.edu>
2018-12-21 13:17:26 -0800

    solve pe294

--- Haskell/pe294.hs
+++ Haskell/pe294.hs
@@ -1,4 +1,6 @@
 
+ten9 = 10^9
+
 -- took me so long to find out: Int overflow
 remainders = map (\x -> fromInteger (((10^x-1) :: Integer) `mod` 23)) [0..21]
 
@@ -13,27 +15,12 @@ assoc_reduce kv_pairs reducer =
 assoc_get kv_pairs key =
   snd . head $ filter ((==key) . fst) kv_pairs
 
--- n choose k mod (10^9)
-ten9 = 10^9 :: Int
-choose_mod :: Int -> Int -> Int
-choose_mod n 0 = 1
-choose_mod n k | k > (n `div` 2) = choose_mod n (n-k)
-choose_mod n k =
-  choose_mod' n k 1 0 0
-  where choose_mod' :: Int -> Int -> Int -> Int -> Int -> Int
-        choose_mod' n 0 accum twos fives =
-          fromInteger ((toInteger accum)
-                       * (2^(toInteger twos) `mod` (toInteger ten9))
-                       * (5^(toInteger fives) `mod` (toInteger ten9))
-                       `mod` (toInteger ten9))
-        choose_mod' n k' accum twos fives =
-          let (n2, n5, n_left) = strip2s5s n 0 0
-              (k2, k5, k_left) = strip2s5s (k-k'+1) 0 0
-          in choose_mod' (n-1) (k'-1) (accum*n_left`div`k_left `mod` ten9)
-             (twos+n2-k2) (fives+n5-k5)
-        strip2s5s n twos fives | n`mod`2 == 0 = strip2s5s (n`div`2) (twos+1) fives
-        strip2s5s n twos fives | n`mod`5 == 0 = strip2s5s (n`div`5) twos (fives+1)
-        strip2s5s n twos fives = (twos, fives, n)
+choose :: Int -> Int -> Integer
+choose n 0 = 1
+choose n k | k > (n `div` 2) = choose n (n-k)
+choose n k = (choose n (k-1)) * toInteger (n-k+1) `div` toInteger k
+
+sum_mod xs = foldl (\acc x -> (acc+x) `mod` ten9) 0 xs
 
 -- partition n with 1..9
 -- not real partition (allow permutation)
@@ -46,12 +33,13 @@ partition_length_memoized = (map partition [0..] !!)
         partition n =
           let recur = [(length+1, count) | i <- [1..9], n-i>=0,
                        (length, count) <- partition_length_memoized (n-i)]
-          in assoc_reduce recur sum
+          in assoc_reduce recur sum_mod
 
+-- TODO: cache this
 partition_maxlen n maxlen =
   let p_lengths = partition_length_memoized n
-  in (sum [count*(choose_mod maxlen length)
-          | (length, count) <- p_lengths, length <= maxlen]) `mod` ten9
+  in sum_mod [count*((choose maxlen length) `mod` ten9) `mod` ten9
+             | (length, count) <- p_lengths, length <= maxlen]
 
 -- remainders R_i
 -- \sum {a_i * R_i} mod 23 = 0
@@ -71,8 +59,9 @@ linear_combo remainders limits maxlens =
   let rems_uses = rem_range 0 (length limits)
   in assoc_get rems_uses (0, 23)
   where
+    rem_range i j | i == j = []
     rem_range i j | j == i+1 =
-                    [((remainders!!i * c `mod` 23, c),
+                    [(((remainders!!i) * c `mod` 23, c),
                       partition_maxlen c (maxlens!!i))
                     | c <- [0..(min (limits!!i) 23)]]
     rem_range i j =
@@ -81,7 +70,7 @@ linear_combo remainders limits maxlens =
       in assoc_reduce [(((rem1+rem2)`mod`23, use1+use2), count1*count2 `mod` ten9)
                       | ((rem1, use1), count1) <- rem_left,
                         ((rem2, use2), count2) <- rem_right,
-                        use1+use2 <= 23] (foldl (\acc x -> (acc+x)`mod`ten9) 0)
+                        use1+use2 <= 23] sum_mod
 
 solve n =
   let (maxlen, rem) = divMod n 22
@@ -95,6 +84,4 @@ main = do
   print $ solve 9
   print $ solve 42
 
-  putStrLn $ "Correct   S(792): " ++ (show $ solve 792)
-  putStrLn $ "Incorrect S(793): " ++ (show $ solve 793)
-  -- print $ solve (11^12)
+  print $ solve (11^12)
---
+++ Python/project_euler294.py
@@ -0,0 +1,72 @@
+# translated from my haskell solution, but caches partition_maxlen
+# NOTE: needs lru_cache (not available in python2)
+
+from collections import defaultdict
+from functools import lru_cache
+
+from mathutil import nCr
+
+
+remainders = [pow(10, x, 23) - 1 for x in range(22)]
+
+
+@lru_cache(maxsize=None)
+def partition_length(n):
+    if n == 0:
+        return {0: 1}
+    res = defaultdict(int)
+    for i in range(1, min(9, n)+1):
+        for length, count in partition_length(n-i).items():
+            res[length+1] += count
+    return res
+
+
+@lru_cache(maxsize=None)
+def partition_maxlen(n, maxlen):
+    total = 0
+    for length, count in partition_length(n).items():
+        if length > maxlen:
+            continue
+        total += count * nCr(maxlen, length)
+    return total
+
+
+def linear_combo(remainders, limits, maxlens):
+    def lc_range(i, j):
+        if i == j:
+            return {}
+        if i+1 == j:
+            return {
+                (remainders[i] * c % 23, c): partition_maxlen(c, maxlens[i])
+                for c in range(min(23, limits[i])+1)
+            }
+
+        res = defaultdict(int)
+        rem_left = lc_range(i, (i+j)//2)
+        rem_right = lc_range((i+j)//2, j)
+        for (rem1, use1), count1 in rem_left.items():
+            for (rem2, use2), count2 in rem_right.items():
+                if use1+use2 > 23:
+                    continue
+                res[((rem1+rem2) % 23, use1+use2)] += count1*count2
+
+        return res
+
+    return lc_range(0, len(limits))[0, 23]
+
+
+def solve(n):
+    maxlen, rem = divmod(n, 22)
+    maxlens = ([maxlen+1] * rem) + ([maxlen] * (22-rem))
+    limits = [ml*9 for ml in maxlens]
+    return linear_combo(remainders, limits, maxlens)
+
+
+def main():
+    # print(solve(9))
+    # print(solve(42))
+    print(solve(11**12))
+
+
+if __name__ == '__main__':
+    main()
